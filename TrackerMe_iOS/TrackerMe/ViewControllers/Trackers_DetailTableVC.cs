09057b5a6c6dba136b5aa3f653a1e0cb34d09ed9
Carlos Iván Ibarra Pacheco <ciip@lux.edu.mx>
2017-05-23 12:40:51 -0500

    Changes on the Folders
    Se re acomodaron los archivos por carpetas y se eliminaron clases
    innecesarias.

---
+++ TrackerMe_iOS/TrackerMe/ViewControllers/Trackers_DetailTableVC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using UIKit;
+
+namespace TrackerMe
+{
+	public partial class Trackers_DetailTableVC : UITableViewController
+	{
+		public Trackers_DetailTableVC(IntPtr handle) : base(handle)
+		{
+		}
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+			btnBack.Clicked += delegate {
+				DismissViewController(true, null);
+			};
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
